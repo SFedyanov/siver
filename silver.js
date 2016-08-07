79dc8fca7abeb41c82447afaf305a859824d4cdf
SFedyanov <sfedyanov@gmail.com>
2016-08-07 21:06:45 +0300

    first commit

---
+++ silver.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/*
+var i;
+for (i=0; i<3; ++i) {
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }
+  console.log(i);
+}
+*/
+
+
+/**
+ * functionComment - description
+ *
+ * @param  {type} argA description
+ * @param  {type} argB description
+ * @param  {type} argC description
+ * @return {type}      description
+ */
+function functionComment (argA, argB, argC) {
+    return 'javadoc';
+}
